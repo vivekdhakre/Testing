2749eaf185dbc1a0be53f040b95dbf25d7c2508b
vivekdhakre <vivek.dhakre6@gmail.com>
2017-05-05 11:08:00 +0530

    updates in office

--- src/Test2feb.java
+++ src/Test2feb.java
@@ -1,8 +1,16 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by vivek on 2/2/17.
  */
 public class Test2feb {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
 //        for(int i=0 ; i<10 ; i++){
 //            System.out.println(i);
@@ -22,10 +30,51 @@ public class Test2feb {
 //            }
 //        }
 
-        Test2feb t = new Test2feb();
-        t.method(null);
+//        DateFormat format = new SimpleDateFormat("dd MMM, yyyy HH:mm:ss");
+//        try {
+//            Date startDate = format.parse("02 may, 2017 18:00:00");
+////            Date endDate = format.parse("2017-05-02 18:00:00");
+//            System.out.println(startDate);
+////            System.out.println(endDate);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+
+
+//        Test2feb t = new Test2feb();
+//        t.method(null);
+
+        String password = "ahoy123";
+
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte byteData[] = md.digest(password.getBytes(StandardCharsets.UTF_8));
+//        md.update(password.getBytes());
+
+//        byte byteData[] = md.digest();
+//        System.out.println(byteData.toString());
+
+
+        //convert the byte to hex format method 1
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+            sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        System.out.println("Hex format : " + sb.toString());
+
+//
+//        //convert the byte to hex format method 2
+//        StringBuffer hexString = new StringBuffer();
+//        for (int i=0;i<byteData.length;i++) {
+//            String hex=Integer.toHexString(0xff & byteData[i]);
+//            if(hex.length()==1) hexString.append('0');
+//            hexString.append(hex);
+//        }
+//        System.out.println("Hex format : " + hexString.toString());
 
     }
+
+
 
     public void method(Object object){
         System.out.println("inside method object");
