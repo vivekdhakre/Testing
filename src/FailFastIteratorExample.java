e05b83e0f4fa75e5f6da9ad86bd826a8cc6d5ced
vivekdhakre <vivek.dhakre6@gmail.com>
2017-05-09 18:47:43 +0530

    failfastvsfailsafe

---
+++ src/FailFastIteratorExample.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by ahoy on 9/5/17.
+ */
+public class FailFastIteratorExample {
+
+    public static void main(String[] args) {
+
+        //Creating an ArrayList of integers
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        //Adding elements to list
+
+        list.add(1452);
+
+        list.add(6854);
+
+        list.add(8741);
+
+        list.add(6542);
+
+        list.add(3845);
+
+        //Getting an Iterator from list
+
+        Iterator<Integer> it = list.iterator();
+
+        while (it.hasNext()) {
+            Integer integer = (Integer) it.next();
+
+            list.add(8457);      //This will throw ConcurrentModificationException
+        }
+
+
+    }
+}
